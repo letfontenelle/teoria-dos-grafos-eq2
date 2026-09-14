86fbcad49cb198b9d18e4b744efc7f56ee1a5c3a
giovannaps <giovannapslima.gpsl@gmail.com>
2026-09-14 16:59:28 -0300

    feat: Adiciona estrutura inicial das tabelas

--- back-end/database/table.py
+++ back-end/database/table.py
@@ -1,11 +1,33 @@
 ##conectar com banco 
 cur = conn.cursor()
 
-def tabela_curso(cur):
+def table_curso(cur):
     cur.execute("""CREATE TABLE curso(
             CURSO_ID SERIAL PRIMARY KEY,
             CURSO_NOME VARCHAR(100) NOT NULL
             )""")
+    
+def table_perfil_curricular(cur):
+    cur.execute("""CREATE TABLE PERFIL_CURRICULAR(
+            PERFIL_ID SERIAL PRIMARY KEY,
+            PERFIL_TIPO VARCHAR(100) NOT NULL
+            )""")
+    
+def table_disciplina(cur):
+    cur.execute("""CREATE TABLE DISCIPLINA(
+            DISCIPLINA_ID SERIAL PRIMARY KEY,
+            DISCIPLINA_CODIGO VARCHAR(100) NOT NULL, 
+            DISCIPLINA_DESCRICAO VARCHAR(100) NOT NULL,
+            DISCIPLINA_PERIODO VARCHAR(100) NOT NULL
+            )""")   
+    
+def table_professor(cur):
+    cur.execute("""CREATE TABLE PROFESSOR(
+            PRO_ID SERIAL PRIMARY KEY,
+            PRO_NOME VARCHAR(100) NOT NULL, 
+            PRO_MATRICULA integer NOT NULL
+            )""")
+                    
 
 
 conn.commit()
