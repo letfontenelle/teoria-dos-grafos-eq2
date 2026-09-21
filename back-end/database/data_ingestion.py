d52654f21c1e26c93fe53ccedf189444e0954dcb
giovannaps <giovannapslima.gpsl@gmail.com>
2026-09-21 07:54:49 -0300

    feat: Arquivos csv para o supabase

--- back-end/database/data_ingestion.py
+++ back-end/database/data_ingestion.py
@@ -1,8 +1,21 @@
 #Adicionar arquivos csv para enviar para as tabelas no supabase
-
 import pandas as pd
 from connect import supabase
 
-# Busca os dados e exibe no formato DataFrame
-res = supabase.table("PROFESSOR").select("*").execute()
-print(res.data)
+tabelas = [
+    ('professor.csv', 'PROFESSOR'),
+    ('disciplina.csv', 'DISCIPLINA'),
+    ('horario.csv', 'HORARIO'),
+    ('alocacao.csv', 'ALOCACAO'),       
+    ('grade_horaria.csv', 'GRADE_HORARIA') 
+]
+
+def csv_supabase(tabelas):
+    for arquivo, tabela in tabelas:
+        df = pd.read_csv(arquivo)
+        df = df.where(pd.notnull(df), None)
+        data = df.to_dict(orient='records')
+        response = supabase.table(tabela).upinsert(data).execute()
+        print(f"Dados inseridos na tabela {tabela}: {response.data}")    
+  
+    
